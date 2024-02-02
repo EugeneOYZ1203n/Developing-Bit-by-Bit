5dd7ee95d2f4260f8ce48cea52ea640cc0d084c3
Eugene Oh Yun Zheng <eugeneoyz.12.03.n@gmail.com>
2024-02-02 20:19:11 +0800

    Connected MongoDB to express

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,10 +6,17 @@ import mongoose from 'mongoose';
 
 import router from './router';
 
+dotenv.config();
+
 const app = express();
 
-app.get('/', (req: Request, res: Response)=>{
-    res.send("Hello world");
-})
+app.use(express.json())
+app.use(cors());
+app.use(morgan('tiny')); 
 
-app.listen(8080);
+app.use(router);
+
+mongoose.connect(process.env.MONGO_URI!).then(()=>{
+    app.listen(8080);
+    console.log("App started listening on 8080")
+})
